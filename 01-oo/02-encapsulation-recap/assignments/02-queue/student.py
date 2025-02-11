1f33aeb30e336e957b27f20e471d46d53993c954
Alek Vernelen <alek.vernelen@student.ucll.be>
2025-02-11 14:37:33 +0100

    02-encapsulation: finished

--- 01-oo/02-encapsulation-recap/assignments/02-queue/student.py
+++ 01-oo/02-encapsulation-recap/assignments/02-queue/student.py
@@ -2,26 +2,18 @@ class Queue():
     def __init__(self):
         self.__queue = []
 
-    @property
-    def queue(self):
-        return self.__queue
-
     def add(self, item):
-        self.queue.append(item)
+        self.__queue.append(item)
     
     def next(self):
-        next = self.queue[0]
-        self.queue.remove(next)
+        next = self.__queue[0]
+        self.__queue.remove(next)
         return next
     
     def is_empty(self):
-        return len(self.queue) == 0
+        return len(self.__queue) == 0
     
 queue = Queue()
 queue.add('Alice')
 queue.add('Bob')
 queue.add('Charlie')
-
-print(queue.next())
-print(queue.next())
-print(queue.next())
