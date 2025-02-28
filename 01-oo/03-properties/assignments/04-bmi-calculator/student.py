0b51b8db1f8e99379fda5ff56bbd30d129b7e9e0
Alek Vernelen <alek.vernelen@student.ucll.be>
2025-02-28 10:26:25 +0100

    03/04-BMI: finished

--- 01-oo/03-properties/assignments/04-bmi-calculator/student.py
+++ 01-oo/03-properties/assignments/04-bmi-calculator/student.py
@@ -1,1 +1,22 @@
-# Write your code here
+class BMICalculator():
+    def __init__(self, weight, height):
+        self.weight = weight
+        self.height = height
+        self.bmi = self.calc_bmi()
+        self.category = self.category()
+
+    def calc_bmi(self):
+        return round(self.weight/(self.height**2), 2)
+    
+    def category(self):
+        if 18.5 <= self.bmi <= 25:
+            return "normal"
+        elif self.bmi < 18.5:
+            return "underweight"
+        else:
+            return "overweight"
+    
+
+calc = BMICalculator(weight=80, height=1.80)
+print(calc.bmi)
+print(calc.category)
